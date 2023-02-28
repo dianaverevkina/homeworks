71de335a16b4d1c23e1b5600f9d575a0c1182e22
Diana Verevkina <diana_verevkina@mail.ru>
2023-02-28 19:43:49 +0300

    tasks done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount(str) {
+  let stringToNumber = Number.parseFloat(str);
+  if (isNaN(stringToNumber)) {
+    throw new Error('Невалидное значение');
+  }
+  return stringToNumber;
+}
+
+function validateCount(str) {
+  try {
+    return parseCount(str);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b < c || b + c < a || c + a < b) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  };
+
+  get area() {
+    let halfOfPerimeter = 0.5 * this.perimeter;
+    let geronArea = Math.sqrt(halfOfPerimeter * (halfOfPerimeter - this.a) * (halfOfPerimeter - this.b) * (halfOfPerimeter - this.c));
+    return Number(geronArea.toFixed(3));
+  }; 
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b , c);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      }
+    };
+  }
+}
+
+
